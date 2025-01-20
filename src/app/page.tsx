fc2473f8f6fd6ef3c8f9ff4d5dcde011fd52f7fc
Odeyemi Tobiloba <odeyemioluwatobiloba11@gmail.com>
2025-01-20 08:36:43 +0100

    Refactoring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,67 +1,101 @@
-"use client";
-
-import StatsCard from "@/components/dashboard/StatsCard";
-import ConsultationsChart from "@/components/dashboard/ConsultationChart";
-import ComparisonChart from "@/components/dashboard/ComparisonChart";
-import OrdersTable from "@/components/dashboard/OrdersTable";
-// import Forecasts from "@/components/dashboard/Forecasts";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="max-w-9xl">
-      <div className="flex justify-between items-center mb-6">
-        <h1 className="text-2xl font-semibold">At a glance</h1>
-        <select className="border rounded-md p-2">
-          <option>7 days</option>
-        </select>
-      </div>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2">
+            Get started by editing{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+              src/app/page.tsx
+            </code>
+            .
+          </li>
+          <li>Save and see your changes instantly.</li>
+        </ol>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-8">
-        <StatsCard
-          title="CONSULTATIONS"
-          value={24}
-          change={15}
-          type="increase"
-        />
-        <StatsCard
-          title="ORDERS PLACED"
-          value={12}
-          change={15}
-          type="decrease"
-        />
-        <StatsCard title="CONVERSION" value="50%" change={15} type="decrease" />
-        <StatsCard
-          title="TOTAL SALES VALUE"
-          value="$2,400"
-          change={15}
-          type="increase"
-        />
-        <StatsCard
-          title="AVG ORDER VALUE"
-          value="$240"
-          change={15}
-          type="increase"
-        />
-        <StatsCard
-          title="COMMISSION PAID"
-          value="$240"
-          change={15}
-          type="increase"
-        />
-      </div>
-
-      <section className="mb-8">
-        <h2 className="text-xl font-semibold mb-6">Insights</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <ConsultationsChart />
-          <ComparisonChart />
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read our docs
+          </a>
         </div>
-      </section>
-
-      <section>
-        <h2 className="text-xl font-semibold mb-6">Orders</h2>
-        <OrdersTable />
-      </section>
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
     </div>
   );
 }
